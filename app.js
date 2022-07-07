f87064cb4b91ba67e0ca0800ebe24dfe097a73ec
santysrodriguez <santiagocarp987@gmail.com>
2022-07-07 17:49:41 -0300

    creacion de la estructura del servidor

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const port = 3030;
+app.use(express.static('public'));
+
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
+
+app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port));
